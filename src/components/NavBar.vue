d1b7fa510d6e98321b5fe38dc17988399def25ce
RacimRgh <racim458@gmail.com>
2021-01-22 19:07:59 +0100

    added navbar section + icons imports

---
+++ src/components/NavBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="level has-background-primary">
+    <div class="level-item has-text-centered">
+      <figure class="image is-128x128">
+        <img class="is-rounded" :src="headerImage" width="150" height="150" />
+      </figure>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="title">{{ mydata.name }}</p>
+        <p class="heading">{{ mydata.age }}</p>
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="title">Contact</p>
+        <b-button
+          type="is-info"
+          icon-pack="fab"
+          size="is-small"
+          icon-left="github"
+          @click="openLinkTab(contact.github)"
+          rounded
+        />
+        <b-button
+          type="is-info"
+          icon-pack="fab"
+          size="is-small"
+          icon-left="linkedin"
+          @click="openLinkTab(contact.linkedin)"
+          rounded
+        />
+        <b-button
+          type="is-info"
+          icon-pack="fas"
+          size="is-small"
+          icon-left="at"
+          @click="openLinkTab(contact.email)"
+          rounded
+        />
+      </div>
+    </div>
+    <div class="level-item has-text-centered">
+      <div>
+        <p class="title">About me</p>
+        <p class="heading">Followers</p>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      mydata: {
+        name: "Racim RIGHI",
+        age: "21 ans",
+      },
+      contact: {
+        email: "racim458@gmail.com",
+        linkedin: "https://www.linkedin.com/in/racim-righi/",
+        github: "https://github.com/RacimRgh",
+      },
+      headerImage: require("../assets/me.png"),
+    };
+  },
+  methods: {
+    openLinkTab(link) {
+      window.open(link, "_blank");
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,53 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+require("@/assets/main.scss");
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+// internal icons
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFontAwesome,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fas);
+library.add(faUserSecret, faGithub, faLinkedin);
+library.add(faFontAwesome);
+Vue.component("vue-fontawesome", FontAwesomeIcon);
+
+Vue.use(Buefy, {
+  defaultIconComponent: "vue-fontawesome",
+  defaultIconPack: "fas",
+  customIconPacks: {
+    fas: {
+      sizes: {
+        default: "lg",
+        "is-small": "2x",
+        "is-medium": "3x",
+        "is-large": "4x",
+      },
+      iconPrefix: "",
+    },
+    fab: {
+      sizes: {
+        default: "lg",
+        "is-small": "2x",
+        "is-medium": "3x",
+        "is-large": "4x",
+      },
+      iconPrefix: "",
+    },
+  },
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
